34e50c743231f5c5fb3dc9d1b1ae0e585a07ceb0
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-05-27 19:38:58 +0300

    Updated main.swift. Add console/file mode selection

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,7 @@
-print("Hello World")
+let consoleModeChosen = CommandLine.arguments.count == 1
+
+let coefficientsSource: CoefficientsSource = consoleModeChosen 
+    ? ConsoleCoefficientsSource() 
+    : FileCoefficientsSource(
+        path: CommandLine.arguments[1]
+    )
