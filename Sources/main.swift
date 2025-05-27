6942102e6dd835b925ef72f3db631dac933e0134
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-05-27 19:41:33 +0300

    Updated main.swift. Add solveQuadraticEquation func calling and result printing

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,3 +5,27 @@ let coefficientsSource: CoefficientsSource = consoleModeChosen
     : FileCoefficientsSource(
         path: CommandLine.arguments[1]
     )
+
+
+let (a, b, c) = coefficientsSource.get()
+let result = solveQuadraticEquation(a, b, c)
+
+print("Equation is: (\(a)) x^2 + (\(b)) x + (\(c)) = 0")
+printEquationResult(result)
+
+func printEquationResult(_ result: QuadraticEquationResult) {
+    if case let .twoRoots(x1, x2) = result {
+        print("There are 2 roots:")
+        print("x1 = \(x1)")
+        print("x2 = \(x2)")
+    }
+
+    if case let .oneRoot(x) = result {
+        print("There is 1 root:")
+        print("x = \(x)")
+    }
+
+    if case .noRoots = result {
+        print("No roots for such coefficients")
+    }
+}
